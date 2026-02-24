9a836fd057ca03844b38f43032640ec8010fa3df
zdarova90210 <zdarova90210@gmail.com>
2026-02-24 22:51:27 +0500

    Add a welcome route and base styles

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,7 @@
 import { Routes } from '@angular/router';
+import {Welcome} from './components/welcome/welcome';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {path: 'welcome', component: Welcome},
+  {path: '', redirectTo: 'welcome', pathMatch: 'full'},
+];
---
+++ src/app/components/welcome/welcome.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-welcome',
+  imports: [],
+  templateUrl: './welcome.html',
+  styleUrl: './welcome.scss',
+})
+export class Welcome {
+
+}
